4747fc7e9a8e46703a5cfd3ad8b6abf1c1012804
Zoe Railing <zoe.railing@gmail.com>
2019-04-17 04:27:42 +0200

    updates

--- 04_Parsing/wordorder.py
+++ 04_Parsing/wordorder.py
@@ -1,3 +1,12 @@
 import pyconll
 
+my_conll_file_location = "./UD_English-GUM/train.conll"
+train = pyconll.load_from_file(my_conll_file_location)
+
+
+for sentence in train:
+	for token in sentence:
+		if token.pos == 'NOUN':
+			noun_token_transormation(token)
+
 
